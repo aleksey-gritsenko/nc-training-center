58de1affc7f59432a7b56f008a4c47fc05e63397
cuckergates23 <35266595+cuckergates23@users.noreply.github.com>
2019-11-02 12:29:42 +0200

    Create user model

---
+++ src/main/java/ua/com/nc/nctrainingproject/model/User.java
@@ -0,0 +1,49 @@
+package ua.com.nc.nctrainingproject.model;
+
+public class User {
+    private String userName;
+    private String userPassword;
+    private String email;
+
+    public User(String userName, String userPassword, String email) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
+    }
+}
