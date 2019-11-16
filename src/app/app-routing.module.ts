343f70c028d70a516fa62317bff5672c1167d4cd
smithros <kovalr2000@gmail.com>
2019-11-16 14:43:06 +0200

    Resolved merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-
-import {LoginComponent} from './login/login.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {LandingComponent} from './landing/landing.component';
-import {ErrorpageComponent} from './errorpage/errorpage.component';
-import {RecoverComponent} from './recover/recover.component';
-import {ChangeComponent} from './change/change.component';
-=======
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
-import {RegistrationComponent} from "./components/registration/registration.component";
-import {LandingComponent} from "./components/landing/landing.component";
-import {ErrorpageComponent} from "./components/errorpage/errorpage.component";
-import {UserProfileComponent} from "./components/user-profile/user-profile.component";
->>>>>>> 6370a182546e95818761b382343272ba4d99af66
+import {LoginComponent} from './components/login/login.component';
+import {RegistrationComponent} from './components/registration/registration.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {ErrorpageComponent} from './components/errorpage/errorpage.component';
+import {UserProfileComponent} from './components/user-profile/user-profile.component';
 
 const routes: Routes = [{
   path: 'login',
   component: LoginComponent
 },
-<<<<<<< HEAD
   {
     path: 'registration',
     component: RegistrationComponent
@@ -32,10 +19,9 @@ const routes: Routes = [{
     component: LandingComponent
   },
   {
-    path: 'recover',
-    component: RecoverComponent
+    path: 'user',
+    component: UserProfileComponent
   },
-
   {
     path: '',
     component: LandingComponent,
@@ -45,39 +31,12 @@ const routes: Routes = [{
     path: '**',
     component: ErrorpageComponent
   }
-
-=======
-{
-path:'registration',
-component:RegistrationComponent
-},
-{
-path:'home',
-component: LandingComponent
-},
-{
-path: 'user',
-component: UserProfileComponent
-},
-{
-path:'',
-component: LandingComponent,
-pathMatch:'full'
-},
-{
-path:'**',
-component:ErrorpageComponent
-}
->>>>>>> 6370a182546e95818761b382343272ba4d99af66
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
+
 export class AppRoutingModule {
 }
-=======
-export class AppRoutingModule { }
->>>>>>> 6370a182546e95818761b382343272ba4d99af66
