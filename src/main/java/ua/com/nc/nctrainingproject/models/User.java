5405e4ee8c0015912e5b920205285519dee25f3b
smithros <kovalr2000@gmail.com>
2019-11-17 22:08:02 +0200

    Fixed User merge conflict

--- src/main/java/ua/com/nc/nctrainingproject/models/User.java
+++ src/main/java/ua/com/nc/nctrainingproject/models/User.java
@@ -1,75 +1,68 @@
 package ua.com.nc.nctrainingproject.models;
 
 public class User extends Entity {
-  private String userName;
-  private String userPassword;
-  private String email;
-  private String recoverCode;
-  private int userRole;
+	private String userName;
+	private String userPassword;
+	private String email;
+	private String recoverCode;
+	private int userRole;
 
-  public User(int id, String userName, String userPassword, String email, String recoverCode, int userRole) {
-    super(id);
-    this.userName = userName;
-    this.userPassword = userPassword;
-    this.email = email;
-    this.recoverCode = recoverCode;
-    this.userRole = userRole;
-  }
+	public User(int id, String userName, String userPassword, String email, String recoverCode, int userRole) {
+		super(id);
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.email = email;
+		this.recoverCode = recoverCode;
+		this.userRole = userRole;
+	}
 
-  public User() {
-    super();
-  }
+	public User() {
+		super();
+	}
 
-<<<<<<< HEAD
-  public User(String newLogin, String newPassword, String newEmail) {
-    this.userName=newLogin;
-    this.userPassword=newPassword;
-    this.email=newEmail;
-=======
-  public User(String userName, String userPassword, String email) {
-    this.userName = userName;
-    this.userPassword = userPassword;
-    this.email = email;
->>>>>>> 138d052c6e33e70be10edad21e3201db95af2e11
-  }
+	public User(String newLogin, String newPassword, String newEmail) {
+		this.userName = newLogin;
+		this.userPassword = newPassword;
+		this.email = newEmail;
+	}
 
-  public String getUserName() {
-    return userName;
-  }
+	public String getUserName() {
+		return userName;
+	}
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-  public String getUserPassword() {
-    return userPassword;
-  }
+	public String getUserPassword() {
+		return userPassword;
+	}
 
-  public void setUserPassword(String userPassword) {
-    this.userPassword = userPassword;
-  }
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public String getRecoverCode() {
-    return recoverCode;
-  }
+	public String getRecoverCode() {
+		return recoverCode;
+	}
 
-  public void setRecoverCode(String recoverCode) {
-    this.recoverCode = recoverCode;
-  }
+	public void setRecoverCode(String recoverCode) {
+		this.recoverCode = recoverCode;
+	}
 
-  public int getUserRole() {
-    return userRole;
-  }
+	public int getUserRole() {
+		return userRole;
+	}
 
-  public void setUserRole(int userRole) {
-    this.userRole = userRole;
-  }
+	public void setUserRole(int userRole) {
+		this.userRole = userRole;
+	}
 }
