9cb0d9196653449df8d0ed3c5ef6f656ca40ddcc
smithros <kovalr2000@gmail.com>
2019-11-19 19:31:28 +0200

    Deleted Admin logic

--- src/main/java/ua/com/nc/nctrainingproject/persistance/dao/AdministratorDAO.java
+++ src/main/java/ua/com/nc/nctrainingproject/persistance/dao/AdministratorDAO.java
@@ -1,7 +1,5 @@
 package ua.com.nc.nctrainingproject.persistance.dao;
 
-import ua.com.nc.nctrainingproject.models.Admin;
-
 public interface AdministratorDAO {
 
     Admin getAdministratorByName(String name);
