dd4361527d8bb2301263507f1f2195527777fd02
cuckergates23 <35266595+cuckergates23@users.noreply.github.com>
2019-11-19 18:36:31 +0200

    add some fixes

--- src/app/services/recover/recover.service.ts
+++ src/app/services/recover/recover.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecoverService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
 
