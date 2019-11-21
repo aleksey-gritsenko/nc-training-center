558b9e7761035174bbbda664ddcc2cf617f1027e
Julia <jalpaeva@gmail.com>
2019-11-21 18:57:45 +0200

    j

--- frontend/src/main/frontend/src/app/components/announcement/announcement.component.ts
+++ frontend/src/main/frontend/src/app/components/announcement/announcement.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Component, Input, OnInit} from '@angular/core';
 import {Announcement} from '../../models/announcement';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'app-announcement',
@@ -8,40 +9,52 @@ import {HttpClient} from '@angular/common/http';
     styleUrls: ['./announcement.component.css']
 })
 export class AnnouncementComponent implements OnInit {
-    model: Announcement = {
-        id: 0,
-        title: '',
-        description: '',
-        date: ''
+    model:Announcement = {
+        title:'',
+        description:'',
+        date:'',
+        bookId:0
     };
-
-    constructor(private http: HttpClient) {
+    ann :Announcement;
+    constructor(private http: HttpClient,private route: ActivatedRoute) {
     }
+    id:any;
 
     /*
       constructor(private route: ActivatedRoute) { }
       id:any;
       ngOnInit() {
         this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-
      */
 
     ngOnInit() {
+        this.model
+        this.id
+            = parseInt(this.route.snapshot.paramMap.get('id'));
     }
 
     createAnnouncement(): void {
-        let url = 'http://localhost:8080/announcement';
+        let url = 'http://localhost:8080/myannouncement';
         let form = new FormData();
         form.append('title', this.model.title);
         form.append('description', this.model.description);
-        form.append('date', this.model.date);
-        this.http.post(url, form).subscribe(
-            res => {
+        form.append('date',this.model.date);
+        /* this.ann .title = this.model.title;
+          this.ann.date = this.model.date;
+          this.ann.description = this.model.description;
+          this.ann.bookId = this.id;
+         */
+
+        // form.append('bookId',this.model.bookId)
+        this.model.bookId = this.id;
+        this.http.post(url, this.model).subscribe(
+            res=>{
                 location.reload();
             },
-            err => {
-                alert(JSON.parse(JSON.stringify(err)).message);
-            }
+            err=>{
+                err => {alert(JSON.parse(JSON.stringify(err)).message);}      }
         );
+
     }
+
 }
