ff6ca74a473ec961847d8dbc68a342b966df3164
smithros <kovalr2000@gmail.com>
2019-11-23 13:19:41 +0200

    Added ActionService and ActionTypeService

---
+++ backend/src/main/java/ua/com/nc/nctrainingproject/services/ActionService.java
@@ -0,0 +1,53 @@
+package ua.com.nc.nctrainingproject.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.com.nc.nctrainingproject.models.Action;
+import ua.com.nc.nctrainingproject.persistance.dao.postgre.ActionPostgreDAO;
+
+import java.util.List;
+
+@Service
+public class ActionService {
+
+	private final ActionPostgreDAO actionPostgreDAO;
+
+	@Autowired
+	public ActionService(ActionPostgreDAO actionPostgreDAO) {
+		this.actionPostgreDAO = actionPostgreDAO;
+	}
+
+	public List<Action> getAllActions() {
+		return actionPostgreDAO.getAllActions();
+	}
+
+	public Action getActionById(int actionId) {
+		return actionPostgreDAO.getActionById(actionId);
+	}
+
+	public Action getActionByUserId(int userId) {
+		return actionPostgreDAO.getActionByUserId(userId);
+	}
+
+	public Action getActionByActionTypeId(int actionTypeId) {
+		return actionPostgreDAO.getActionByActionTypeId(actionTypeId);
+	}
+
+	void deleteActionByActionId(int actionId) {
+		if (actionPostgreDAO.getActionById(actionId) != null) {
+			actionPostgreDAO.deleteActionByActionId(actionId);
+		}
+	}
+
+	void createAction(Action action) {
+		if (actionPostgreDAO.getActionById(action.getActionId()) != null) {
+			actionPostgreDAO.createAction(action);
+		}
+	}
+
+	void updateAction(int actionId, Action action) {
+		if (actionPostgreDAO.getActionById(actionId) != null) {
+			actionPostgreDAO.updateAction(actionId, action);
+		}
+	}
+}
