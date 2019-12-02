943809441edd9490c6cd0e554a90dee1603db00b
smithros <kovalr2000@gmail.com>
2019-12-02 18:03:24 +0200

    Delete import

--- backend/src/main/java/ua/com/nc/nctrainingproject/persistance/dao/postgre/BookImagePostgreDAO.java
+++ backend/src/main/java/ua/com/nc/nctrainingproject/persistance/dao/postgre/BookImagePostgreDAO.java
@@ -1,6 +1,5 @@
 package ua.com.nc.nctrainingproject.persistance.dao.postgre;
 
-import com.sun.org.apache.xpath.internal.objects.XNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ua.com.nc.nctrainingproject.models.Book;
