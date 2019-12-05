e29f4b3584f723e208bba6c76cd98a9f1a4679a3
cuckergates23 <35266595+cuckergates23@users.noreply.github.com>
2019-12-05 23:11:01 +0200

    added rights dao

---
+++ backend/src/main/java/ua/com/nc/nctrainingproject/persistance/dao/postgre/RightsPostgreDAO.java
@@ -0,0 +1,20 @@
+package ua.com.nc.nctrainingproject.persistance.dao.postgre;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ua.com.nc.nctrainingproject.persistance.dao.postgre.queries.RightsQuery;
+
+@Repository
+public class RightsPostgreDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public RightsPostgreDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public String getDescriptionByRightId(int rightId) {
+       return jdbcTemplate.queryForObject(RightsQuery.GET_DESCRIPTION_BY_RIGHT_ID, new Object[]{rightId}, String.class);
+    }
+}
