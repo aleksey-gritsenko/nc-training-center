6dec38377a29d3563b23f58cca013639b1a9e300
kandel_k <kirill.kandel@gmail.com>
2019-12-10 22:00:22 +0200

    Added session storage for user

--- frontend/src/main/frontend/src/app/components/authorization/login/login.component.ts
+++ frontend/src/main/frontend/src/app/components/authorization/login/login.component.ts
@@ -1,18 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {LogView} from '../../../models/logview';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
-
 import {AuthenticationService} from '../../../services/authentification/authentication.service';
 import {StorageService} from "../../../services/storage/storage.service";
 import {SpringAuthService} from "../../../services/security/spring-auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['../../../resources/styles/Authorization.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
     model: LogView = {
         id: 0,
         login: '',
@@ -21,7 +21,9 @@ export class LoginComponent implements OnInit {
 
     isError: boolean = false;
     returnUrl: string;
-    errorMessage = '';
+
+    subscription: Subscription;
+    loginSubscription: Subscription;
 
     constructor(
         private http: HttpClient, public serv: AuthenticationService,
@@ -35,17 +37,31 @@ export class LoginComponent implements OnInit {
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+        if (this.loginSubscription) {
+            this.loginSubscription.unsubscribe();
+        }
+    }
+
     login(): void {
-        this.springAuth.authentificate(this.model.login, this.model.password).subscribe(
+        this.subscription = this.springAuth.authentificate(this.model.login, this.model.password).subscribe(
             data => {
                 window.sessionStorage.setItem('token', JSON.stringify(data));
-                if (this.model.login.length == 0 || this.model.password.length == 0) return;
-                this.serv.login(this.model.login, this.model.password)
-                    .subscribe(
+
+                if (this.model.login.length == 0 || this.model.password.length == 0) {
+                    return;
+                }
+                this.loginSubscription = this.serv.login(this.model.login, this.model.password).subscribe(
                         user => {
-                            if (!user.verified) this.router.navigateByUrl('/verify');
+                            if (!user.verified) {
+                                this.router.navigateByUrl('/verify');
+                            }
                             else {
                                 this.storageService.setUser(user);
+                                sessionStorage.setItem('user', JSON.stringify(user)); //TODO Should store encrypted user
                                 this.router.navigate([this.returnUrl]);
                             }
                         });
@@ -55,6 +71,5 @@ export class LoginComponent implements OnInit {
                 this.model.password = '';
             }
         );
-
     }
 }
