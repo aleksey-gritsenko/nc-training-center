7f85e32a5b4e9cd2ba7e46b5e8d8b88d4d6084ef
kandel_k <kirill.kandel@gmail.com>
2019-12-13 22:01:32 +0200

    Added form to the registration page.

--- frontend/src/main/frontend/src/app/components/authorization/registration/registration.component.ts
+++ frontend/src/main/frontend/src/app/components/authorization/registration/registration.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {RegView} from '../../../models/regview';
 import {HttpClient} from '@angular/common/http';
 import {Router} from "@angular/router";
 import {StorageService} from "../../../services/storage/storage.service";
 import {AuthenticationService} from "../../../services/authentification/authentication.service";
 import {Subscription} from "rxjs";
 import {SpringAuthService} from "../../../services/security/spring-auth.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../services/user/user.service";
 
 @Component({
     selector: 'app-registration',
@@ -13,49 +14,58 @@ import {SpringAuthService} from "../../../services/security/spring-auth.service"
     styleUrls: ['../../../resources/styles/Authorization.css']
 })
 export class RegistrationComponent implements OnInit, OnDestroy {
-    model: RegView = {
-        id: 0,
-        login: '',
-        password: '',
-        email: ''
-    };
+    regGroup: FormGroup;
 
-    subscription: Subscription;
+    regSubscription: Subscription;
+    securitySubscription: Subscription;
+
     isError: boolean;
-    confirmPassword: string = '';
 
     constructor(private http: HttpClient,
                 private router: Router,
                 private storage: StorageService,
                 private authenticationService: AuthenticationService,
-                private springAuth: SpringAuthService) {
+                private springAuth: SpringAuthService,
+                private userService: UserService) {
     }
 
     ngOnInit() {
+        this.regGroup = new FormGroup({
+            username: new FormControl('', [Validators.minLength(4), Validators.pattern(this.userService.inputRegExp)]),
+            email: new FormControl('', [Validators.pattern(this.userService.emailRegExp)]),
+            password: new FormControl('', Validators.pattern(this.userService.passwordRegExp)),
+            confirmPassword: new FormControl('', Validators.pattern(this.userService.passwordRegExp))
+        });
     }
 
+    get username() {return this.regGroup.get('username');}
+    get password() {return this.regGroup.get('password');}
+    get email() {return this.regGroup.get('email');}
+    get confirmPassword() {return this.regGroup.get('confirmPassword');}
+
     register(): void {
-        this.subscription = this.authenticationService.register(this.model.login, this.model.password, this.model.email).subscribe(
+        this.isError = false;
+        let username = this.username.value;
+        let password = this.password.value;
+
+        this.securitySubscription = this.authenticationService.register(username, password, this.email.value).subscribe(
             res => {
-                this.springAuth.authentificate(this.model.login, this.model.password).subscribe(
+                this.securitySubscription = this.springAuth.authentificate(username, password).subscribe(
                     data => {
                         window.sessionStorage.setItem('token', JSON.stringify(data));
-                        alert('success registration');
-                    },
-                    err => {
-                        alert('Registration error!!');
-                    }
-                );
+                    });
                 this.storage.setUser(res);
-                this.router.navigateByUrl('/activate');
+                this.router.navigateByUrl('/verify');
             },
             err => {
                 this.isError = true;
+                this.regGroup.patchValue({password: '', confirmPassword: ''});
             }
         );
     }
 
     ngOnDestroy(): void {
-        if (this.subscription) this.subscription.unsubscribe();
+        if (this.regSubscription) this.regSubscription.unsubscribe();
+        if (this.securitySubscription) this.securitySubscription.unsubscribe();
     }
 }
